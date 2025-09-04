48c0bc0fa480b319ed23fa6f1f37251e9fff50a5
GarvvB <garvb1404@gmail.com>
2025-09-04 14:06:54 +0530

    final: commit

--- newapp/seller.py
+++ newapp/seller.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, SellerRegisterForm
+from .forms import UserRegisterForm, SellerProfileForm  # <-- Fix here
 from .models import SellerProfile, Product
 
 def seller_register(request):
     if request.method == 'POST':
         user_form = UserRegisterForm(request.POST)
-        seller_form = SellerRegisterForm(request.POST)
+        seller_form = SellerProfileForm(request.POST)  # <-- Fix here
         if user_form.is_valid() and seller_form.is_valid():
             user = user_form.save(commit=False)
             user.set_password(user_form.cleaned_data['password'])
@@ -22,7 +22,7 @@ def seller_register(request):
             return redirect('sellerpage')
     else:
         user_form = UserRegisterForm()
-        seller_form = SellerRegisterForm()
+        seller_form = SellerProfileForm()  # <-- Fix here
     return render(request, 'seller_register.html', {
         'user_form': user_form, 'seller_form': seller_form
     })
